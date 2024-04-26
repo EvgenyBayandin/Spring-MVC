485f8b1b76d5ad90ef377a7225030b432636f9df
Evgeny Bayandin <5087081@gmail.com>
2024-04-26 21:16:33 +0300

    Added FirstController.java in the Spring MVC App1 project

--- src/main/java/controllers/HelloController.java
+++ src/main/java/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HelloController {
     @GetMapping("/hello-world")
