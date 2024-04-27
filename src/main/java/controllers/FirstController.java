1f402921a80915369516450901a613a67c825050
Evgeny Bayandin <5087081@gmail.com>
2024-04-27 22:42:19 +0300

    Added Model in FirstController in the Spring MVC App1 project

--- src/main/java/controllers/FirstController.java
+++ src/main/java/controllers/FirstController.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/first") // добавляется префикс /first/hello или /first/goodbye
 public class FirstController {
 
     @GetMapping("/hello")
-    public  String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname){
+    public  String helloPage(@RequestParam(value = "name", required = false) String name,
+                             @RequestParam(value = "surname", required = false) String surname,
+                             Model model){
 
-        System.out.println("Hello " + name + " " + surname);
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
