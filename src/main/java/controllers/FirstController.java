9c2d84d510a9f4cb436dd86863f0aac3543e76f0
Evgeny Bayandin <5087081@gmail.com>
2024-04-27 22:29:03 +0300

    Update FirstController  in the Spring MVC App1 project

--- src/main/java/controllers/FirstController.java
+++ src/main/java/controllers/FirstController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
     @GetMapping("/hello")
-    public  String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname){
+    public  String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname){
 
         System.out.println("Hello " + name + " " + surname);
         return "first/hello";
