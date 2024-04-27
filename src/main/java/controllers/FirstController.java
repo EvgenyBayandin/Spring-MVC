4f3c9e13ea63057d74a760990972394433a4f47e
Evgeny Bayandin <5087081@gmail.com>
2024-04-27 22:23:05 +0300

    Added HttpServletRequest FirstController  in the Spring MVC App1 project

--- src/main/java/controllers/FirstController.java
+++ src/main/java/controllers/FirstController.java
@@ -2,12 +2,18 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/first") // добавляется префикс /first/hello или /first/goodbye
 public class FirstController {
 
     @GetMapping("/hello")
-    public  String helloPage(){
+    public  String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
